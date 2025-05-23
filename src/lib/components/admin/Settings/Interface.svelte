86d4520eb9511e4bc0579f9edff54f913effc52d
SongAoxiang <1849949850@qq.com>
2025-05-23 21:57:54 +0800

    test

--- src/lib/components/admin/Settings/Interface.svelte
+++ src/lib/components/admin/Settings/Interface.svelte
@@ -38,7 +38,16 @@
 		TOOLS_FUNCTION_CALLING_PROMPT_TEMPLATE: ''
 	};
 
-	let promptSuggestions = [];
+	let promptSuggestions = [
+		{
+    title: ['标题1', '副标题1'],  // 可选
+    content: '建议内容1'
+  },
+  {
+    title: ['标题2', '副标题2'],
+    content: '建议内容2'
+  }
+	];
 	let banners: Banner[] = [];
 
 	const updateInterfaceHandler = async () => {
